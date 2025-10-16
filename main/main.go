14b6f50b71c5e612f0e2ea0de457b24a8a677905
Fuad Daoud <fuad.daoud@icloud.com>
2025-10-16 20:54:28 +0300

    feat: lowered fps and added pprof for profiling memory

--- main/main.go
+++ main/main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
-	tea "github.com/charmbracelet/bubbletea"
 	"os"
 	"pkgmate/ui"
+
+	"net/http"
+	_ "net/http/pprof"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	p := tea.NewProgram(ui.InitialModel(), tea.WithAltScreen())
+	// for memory profiling
+	go func() {
+		http.ListenAndServe("localhost:6060", nil)
+	}()
+
+	p := tea.NewProgram(ui.InitialModel(), tea.WithAltScreen(), tea.WithFPS(24))
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
